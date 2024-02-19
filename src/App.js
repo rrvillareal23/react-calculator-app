24a181b223a0f73ee1b096e5dd8b7d533fa0920e
rrvillareal23 <rrvillareal@arizona.edu.com>
2024-02-19 14:20:57 -0800

    made calculator template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css"
 
 function App() {
-  return null
+  return (
+    <div className='calculator-grid'>
+      <div className='output'>
+        <div className='previous-operand'>123,456 *</div>
+        <div className='current-operand'>987,654</div>
+      </div>
+
+      <button className='span-two'>AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>x</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className='span-two'>=</button>
+    
+    </div>
+  ) 
+    
 }
 
 export default App;
