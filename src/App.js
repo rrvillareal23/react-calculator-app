a50e03066607b4aabca91a0f38d462d328b21a63
rrvillareal23 <rrvillareal@arizona.edu.com>
2024-02-19 16:46:22 -0800

    Added formatter to add commas and not mess up decimal numbers.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,13 @@ export const ACTIONS = {
 function reducer(state, {type, payload}) {
   switch(type){
     case ACTIONS.ADD_DIGIT:
+      if(state.overwrite){
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        }
+      }
       if(payload.digit === "0" && state.currentOperand === "0") {
         return state
       }
@@ -55,7 +62,54 @@ function reducer(state, {type, payload}) {
 
     case ACTIONS.CLEAR:
       return {}
+
+    case ACTIONS.DELETE_DIGIT:
+      if(state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null
+        }
+      }
+
+      if (state.currentOperand == null) return state
+      if (state.currentOperand.length === 1) {
+        return {...state, currentOperand: null }
+      }
+
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0,-1)
+
+      }
+    
+    case ACTIONS.EVALUATE:
+      if( 
+        state.operation == null || 
+        state.currentOperand == null || 
+        state.previousOperand == null) {
+        return state
+      }
+
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state)
+      }
   }
+}
+
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us" , {
+  maximumFractionDigits: 0,
+}) 
+
+function formatOperand(operand) {
+  if (operand == null) return
+  const [integer, decimal] = operand.split('.')
+  if (decimal == null) return INTEGER_FORMATTER.format(integer)
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
 }
 
 function evaluate ({ currentOperand, previousOperand, operation }) {
@@ -94,12 +148,12 @@ function App() {
   return (
     <div className='calculator-grid'>
       <div className='output'>
-        <div className='previous-operand'>{previousOperand} {operation}</div>
-        <div className='current-operand'> {currentOperand} </div>
+        <div className='previous-operand'>{formatOperand(previousOperand)} {operation}</div>
+        <div className='current-operand'> {formatOperand(currentOperand)} </div>
       </div>
 
       <button className='span-two' onClick={() => dispatch( {type: ACTIONS.CLEAR})}>AC</button>
-      <button>DEL</button>
+      <button onClick={() => dispatch( {type: ACTIONS.DELETE_DIGIT})}>DEL</button>
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
@@ -115,7 +169,7 @@ function App() {
       <OperationButton operation="-" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
-      <button className='span-two'>=</button>
+      <button className='span-two' onClick={() => dispatch( {type: ACTIONS.EVALUATE})}>=</button>
     
     </div>
   ) 
